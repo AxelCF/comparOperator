7e9a78746b8c93a64f470551d965ba250fbd83b2
adriennnnn <95212982+adriennnnn@users.noreply.github.com>
2022-04-04 15:23:48 +0200

    ADDD

--- page.php
+++ page.php
@@ -4,30 +4,30 @@ include './util/connection.php';
 include './util/autoload.php';
 $manager = new Manager;
 $rltOperator = $manager->getOperatorsByLocation($_GET['location']);
+
+
 // var_dump($rltOperator);
 // die;*
 
 ?>
 <div class="bg-orange-200">
-    <h1 class="md:text-2xl text-orange-700">Pour <?= $_GET['location'] ?></h1>
-
-    <img src="" alt="imageLocation" class="rounded-t-lg md:w-1/3 md:rounded-l-lg md:rounded-t-none">
+    <h1 class="md:text-2xl text-orange-700 block font-bold md:ml-24 ml-0 mb-6 pt-6">Pour <?= $_GET['location'] ?></h1>
 
 
-    <h1 class="text-orange-700">liste bdes opperateur pour cette destination</h1>
+
+    <h1 class="text-orange-700 md:ml-24 ml-0">liste bdes opperateur pour cette destination</h1>
     <?php
     foreach ($rltOperator as $rlt) {
     ?>
         <div class="p-10 ">
             <h3 class="mb-4 font-bold text-orange-400"> </h3>
             <div class="bg-white rounded-lg hover:shadow-2xl ease-in duration-150 md:flex">
-                        <img 
-class="rounded-t-lg md:w-1/3 md:rounded-l-lg md:rounded-t-none" src="<?= $rlt->imageTour() ?>" alt="">                
-              <div class="p-6">
+                <img class="rounded-t-lg md:w-1/3 md:rounded-l-lg md:rounded-t-none" src="<?= $rlt->imageTour() ?>" alt="">
+                <div class="p-6">
                     <h2 class="mb-2 font-bold text:xl md:text-2xl text-orange-700"></h2>
                     <?php if ($rlt->getIsPremium() == 1) {
                     ?>
-                        <p class="text-orange-700"><?= $rlt->getLink() ?></p>
+                        <p class="text-orange-700"><a href="<?= $rlt->getLink() ?>"><?= $rlt->getLink() ?></a></p>
                     <?php }  ?>
                 </div>
             </div>
