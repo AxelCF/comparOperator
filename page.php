fcbb0fe23dbb40dd0d77382079182d7a6c6b5138
AxelCF <95211943+AxelCF@users.noreply.github.com>
2022-04-04 14:11:21 +0200

    maj

--- page.php
+++ page.php
@@ -4,7 +4,7 @@ include './util/connection.php';
 include './util/autoload.php';
 $manager = new Manager;
 $rltOperator = $manager->getOperatorsByLocation($_GET['location']);
-var_dump($rltOperator);
+// var_dump($rltOperator);
 // die;*
 
 ?>
@@ -15,7 +15,9 @@ var_dump($rltOperator);
                 <div class="p-10 ">
                     <h3 class="mb-4 font-bold text-orange-400"> </h3>
                     <div class="bg-white rounded-lg hover:shadow-2xl ease-in duration-150 md:flex">
-                        <img src="<?= $rlt->getName() ?>" alt="Boat" class="rounded-t-lg md:w-1/3 md:rounded-l-lg md:rounded-t-none">
+                        <?= $rlt->getName() ?>
+                        <img 
+class="rounded-t-lg md:w-1/3 md:rounded-l-lg md:rounded-t-none" src="<?= $rlt->imageTour() ?>" alt="">
                         <div class="p-6">
                             <h2 class="mb-2 font-bold text:xl md:text-2xl text-orange-700"></h2>
                             <p class="text-orange-700"><?= $rlt->getLink() ?></p>
