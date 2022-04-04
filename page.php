debd5dec59fc5600733e412223d73ce865ea80d6
AxelCF <95211943+AxelCF@users.noreply.github.com>
2022-04-04 11:43:36 +0200

    maj page

--- page.php
+++ page.php
@@ -4,4 +4,18 @@ include './util/connection.php';
 include './util/autoload.php';
 $manager = new Manager;
 $rltOperator = $manager->getOperatorByDestination($_GET['id']);
+// var_dump($rltOperator);
+// die;*
+
 ?>
+
+                <div class="p-10 ">
+                    <h3 class="mb-4 font-bold text-orange-400"> </h3>
+                    <div class="bg-white rounded-lg hover:shadow-2xl ease-in duration-150 md:flex">
+                        <img src="<?= $rltOperator->getName() ?>" alt="Boat" class="rounded-t-lg md:w-1/3 md:rounded-l-lg md:rounded-t-none">
+                        <div class="p-6">
+                            <h2 class="mb-2 font-bold text:xl md:text-2xl text-orange-700"></h2>
+                            <p class="text-orange-700"><?= $rltOperator->getLink() ?></p>
+                        </div>
+                    </div>
+                </div>
