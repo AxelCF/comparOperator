d027e457af13755b926f3504cf246da02ad3a5ee
adriennnnn <95212982+adriennnnn@users.noreply.github.com>
2022-04-05 11:36:05 +0200

    un

--- class/Manager.php
+++ class/Manager.php
@@ -12,7 +12,7 @@ class Manager{
     }
 
     function getAllDestination(){
-        $result = $this->bdd->query('SELECT * FROM destination GROUP BY location ORDER BY price ASC');');
+        $result = $this->bdd->query('SELECT * FROM destination GROUP BY location ORDER BY price ASC');
         $result = $result->fetchAll(PDO::FETCH_ASSOC);
         $allDestination = [];
         foreach($result as $rlt){
