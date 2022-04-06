a36e92a0f1309a0ad0520bf8678366693c19fb53
AxelCF <95211943+AxelCF@users.noreply.github.com>
2022-04-06 13:56:08 +0200

    mise a jkour id

---
+++ process/primeUpdate.php
@@ -0,0 +1,11 @@
+<?php
+include '../partials/header.php';
+include '../util/connection.php';
+include '../util/autoload.php';
+$manager = new Manager;
+var_dump($_POST['operatorid']);
+
+if(isset($_POST['operatorid'])){
+    $manager->UpdateOperatorToPremium($_POST['operatorid']);
+    header('Location: ../adm.php');
+}
