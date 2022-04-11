68da307fda2aadb72433d9fd8cf76a9658ded266
AxelCF <95211943+AxelCF@users.noreply.github.com>
2022-04-11 15:26:56 +0200

    maj composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8ee4dd50df4c6d6a484bb13a8ae9ee5116453cda',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8ee4dd50df4c6d6a484bb13a8ae9ee5116453cda',
+            'dev_requirement' => false,
+        ),
+    ),
+);
